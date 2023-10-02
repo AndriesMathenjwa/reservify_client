976b54d8b4ade5f1727e7c0b468f01e0dd746d49
Andries Mathenjwa <andriesmathenjwa52@gmail.com>
2023-10-02 02:04:49 +0200

    media queries updates

--- src/componets/header/header.jsx
+++ src/componets/header/header.jsx
@@ -89,7 +89,7 @@ import { AuthContext } from "../../context/AuthContext";
               </h1>
               <p className="headerDesc">
                 Get rewarded for your travels – unlock instant savings of 10% or
-                more with a free MathenjwaFamBooking account
+                more with a free Reservify account
               </p>
               { !user && <button className="headerBtn">Sign in / Register</button> }
               <div className="headerSearch">
